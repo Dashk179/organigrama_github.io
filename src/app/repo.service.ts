0c2a571d6b57dfc59f35b48dabce9b0c66666559
Arturo Perez <dash_arturo@hotmail.com>
2022-10-03 11:27:35 -0500

    Almacenamos los datos dentro del navegador del usuario con IndexedDB

--- src/app/repo.service.ts
+++ src/app/repo.service.ts
@@ -5,8 +5,25 @@ import { OrgaItem } from './_models/OrgaItem';
   providedIn: 'root'
 })
 export class RepoService {
+  DB: Promise<IDBDatabase>;
+  DB_NAME = 'org-data';
+  DB_VERSION = 1;
+  STORAGE_NAME = 'data';
 
-  constructor() { }
+  constructor() {
+    this.DB = new Promise((resolve,reject) =>{
+      const request = window.indexedDB.open(this.DB_NAME,this.DB_VERSION);
+      request.onsuccess = () => resolve(request.result);
+      request.onerror = (e) => reject(e);
+      request.onupgradeneeded = () => {
+        const store = request.result.createObjectStore(this.STORAGE_NAME,{
+          keyPath: 'id',
+          autoIncrement: true
+        });
+        store.createIndex('root','root',{unique:false});
+      };
+    });
+   }
 
   getItem(id: number){}
 
